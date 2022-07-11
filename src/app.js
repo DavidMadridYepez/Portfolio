ff4f1b9b67b745c2809fd8ff52b1a107f2d4c2fe
David Madrid <davidmadridpathway@gmail.com>
2022-07-11 16:24:14 -0500

    Solving working.js problem

--- src/app.js
+++ src/app.js
@@ -1,23 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
-import Home from './pages/home'
-import About from './pages/about'
-import Portfolio from './pages/portfolio'
+import { Working } from './components/working'
 
 function App() {
   return (
-    <div className='bg-gray-900 px-7 md:px-20 xl:px-7'>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/portfolio' element={<Portfolio />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+    <div>
+      <Working />
     </div>
   )
 }
---
+++ src/components/working.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Working = () => {
+  return (
+    <div>
+      <img src="./public/images/arrow.png" alt="" />
+    </div>
+  )
+}
